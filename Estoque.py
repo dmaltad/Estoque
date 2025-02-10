aff81dc8787af50f0e5599a95aacb701c8c1f3d2
Daniel Malta <116168952+dmaltad@users.noreply.github.com>
2025-02-10 08:03:16 -0300

    Update Estoque.py

--- Estoque.py
+++ Estoque.py
@@ -22,37 +22,37 @@ def adicionar(produtos, nome):
         preco = float(input("Preço do produto: "))
         quantidade = int(input("Quantidade do produto: "))
         produtos[nome] = {"quantidade":quantidade, "preco": preco}
-        print(f"O produto {nome} foi adicionado ao estoque.\n")
+        print(f"O produto {nome} foi adicionado ao estoque.")
     else:
-        print("Produto já existe em estoque.\n")
+        print("Produto já existe em estoque.")
 
 def atualizar(produtos, nome):
     if nome in produtos:
-        atualizar = int(input("Digite o número para atualizar:\n1- Preço\n2- Quantidade\n"))
+        atualizar = int(input("Digite o número para atualizar:\n1. Preço\n2. Quantidade\n\n"))
         if atualizar == 1:
-            print(f"Preço: {produtos[nome]["preco"]}.")
-            preco = float(input("Novo preço para o  produto: "))
+            print(f"\nPreço: {produtos[nome]["preco"]}.")
+            preco = float(input("Novo preço: "))
             produtos[nome]["preco"] = preco
-            print(f"O preço do produto {nome} foi atualizado.\n")
+            print(f"O preço do produto {nome} foi atualizado.")
         if atualizar == 2:
-            print(f"Quantidade: {produtos[nome]["quantidade"]}.")
-            quantidade = int(input("Nova quantidade para o produto: "))
+            print(f"\nQuantidade: {produtos[nome]["quantidade"]}")
+            quantidade = int(input("Nova quantidade: "))
             produtos[nome]["quantidade"] = quantidade
-            print(f"A quantidade do produto {nome} foi atualizado.\n")
+            print(f"A quantidade do produto {nome} foi atualizado.")
         if atualizar < 1 or atualizar > 2:
-            print("Opção inválida!\n")
+            print("\nOpção inválida!")
     else:
-        print("Produto inválido!\n")
+        print("\nProduto inválido!")
 
 def remover(produtos, nome):
     if nome in produtos:
         del produtos[nome]
-        print("Produto removido do estoque.\n")
+        print(f"Produto {nome} removido do estoque.")
     else:
-        print("Produto inválido!\n")
+        print("\nProduto inválido!")
 
 def exibir():
-    print("Produtos em estoque:\n")
+    print("\nProdutos em estoque:\n")
     for nome, precoEquantidade in produtos.items():
         print("Nome: " + nome)
         print("Preço: " + str (precoEquantidade["preco"]))
@@ -62,21 +62,23 @@ def calcular():
     c = 0
     for a,b in produtos.items():
        c+=(b["preco"])*(b["quantidade"])
-    print(f"Valor total do estoque: {c}\n")
+    print(f"\nValor total do estoque: {c}")
 
 gerenciar = 0
 
 while gerenciar > 0 or gerenciar < 7:
-    gerenciar = int(input("Gerenciador de Estoque\n1. Adicionar produto\n2. Atualizar produto\n3. Remover produto\n4. Exibir estoque\n5. Calcular valor total do estoque\n6. Sair\n"))
+    gerenciar = int(input("\nGerenciador de Estoque\n1. Adicionar produto\n2. Atualizar produto\n3. Remover produto\n4. Exibir estoque\n5. Calcular valor total do estoque\n6. Sair\n\n"))
     if gerenciar == 1:
-        adicionar(produtos, nome =  input("Nome do produto: "))
+        adicionar(produtos, nome =  input("\nNome do produto: "))
     elif gerenciar == 2:
-        atualizar(produtos, nome = input("Nome do produto: "))
+        atualizar(produtos, nome = input("\nNome do produto: "))
     elif gerenciar == 3:
-        remover(produtos, nome = input("Nome do produto: "))
+        remover(produtos, nome = input("\nNome do produto: "))
     elif gerenciar == 4:
         exibir()
     elif gerenciar == 5:
         calcular()
     elif gerenciar == 6:
         break
+    else:
+        print("\nOpção inserida inválida!")
